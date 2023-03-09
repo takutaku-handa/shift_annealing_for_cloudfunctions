51616726c51b63bd13083ce586c70a6926d0d7cd
takutaku-handa <takutaku.handa27@gmail.com>
2023-03-09 11:50:56 +0900

    LINE Bot向けからWebAPI向けに方針変更

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-import csv
-import math
+import json
+
 import numpy as np
 import dimod
-import time
 from neal import SimulatedAnnealingSampler
-from google.cloud import storage
-from linebot import LineBotApi
-from linebot.models import TextSendMessage, ImageSendMessage
-import matplotlib.pyplot as plt
-
-channel_access_token = "xGrW44hCkpMrzQ58fWVS3ZPAHEA+z7UOLHikUvMO6u592F1F+aTcxKURKx3+CFTT5nu/TTVlzV/I1XlRYiR6lrY7TReIRfLd9AARkClP7CIY5HEezWECcxApeXveX9cuh2RV2Vjqq8P5zeNEjah9XgdB04t89/1O/w1cDnyilFU="
-user_id = "Ufa2bdd3e5bad5382d047a1c23c22cf71"
-bucket_name = "shift_sa"
-
-
-def hello_world(request):
-    request_json = request.get_json()
-    if request_json and "list" in request_json and "param" in request_json:
-        main(request_json["list"], request_json["param"])
-        return "Success!"
-    else:
-        print("No List!")
-        return "Error!"
 
 
 class ShiftAnneal:
@@ -45,17 +26,6 @@ class ShiftAnneal:
 
     def getID(self, m, d):
         return self.DAY * m + d + 100000000
-
-    def setCSV(self, filename):
-        with open(filename, encoding="utf-8-sig") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                self.NAME.append(row[0])
-                self.WORKDAY.append(int(row[-1]))
-                self.const.append([int(i) for i in row[1: -1]])
-
-        self.MANPOWER = len(self.const)
-        self.DAY = len(self.const[0])
 
     def setLIST(self, data_list: list):
         for row in data_list:
@@ -144,187 +114,36 @@ class ShiftAnneal:
                                             num_sweeps_per_beta=100, num_sweeps=10000)
         self.order = np.argsort(self.sample_set.record["energy"])
 
-    def getPenalty(self, sample):
-        # ペナルティ：出勤希望度違反数、昼夜連勤数、人数超過日数、人数不足日数、勤務日のばらつき、勤務日数のばらつき
-        pena_desire = [0, 0, 0, 0]
-        pena_seq = 0
-        pena_over = 0
-        pena_lack = 0
-        pena_dist = 0
-        pena_workday = 0
-
-        count_horizontal = [0 for _ in range(self.MANPOWER)]
-        count_vertical = [0 for _ in range(self.DAY)]
-        max_seq_work = 0
-        max_seq_off = 0
-
-        index = 0
-        for m in range(self.MANPOWER):
-            work_date = []
-            date = 1
-            seq_work = 0
-            seq_off = 0
-            for d in range(self.DAY):
-                s = sample[index]
-                if s:
-                    work_date.append(date)
-                    seq_work += 1
-                    seq_off = 0
-                else:
-                    seq_work = 0
-                    seq_off += 1
-                if seq_work > max_seq_work:
-                    max_seq_work = seq_work
-                if seq_off > max_seq_off:
-                    max_seq_off = seq_off
-                date += 1
-                pena_desire[self.const[m][d]] += s
-                if (d % self.DAY) % 2 == 0 and d < self.DAY and s * sample[index + 1]:
-                    pena_seq += 1
-                count_horizontal[m] += s
-                count_vertical[d] += s
-                index += 1
-            pena_dist += math.sqrt(np.var(work_date))
-
-        for cv in range(self.DAY):
-            if count_vertical[cv] > self.SHIFT_SIZE_LIMIT[cv]:
-                pena_over += 1
-            if count_vertical[cv] < self.SHIFT_SIZE_LIMIT[cv]:
-                pena_lack += 1
-
-        for m in range(self.MANPOWER):
-            gap = (self.WORKDAY[m] - count_horizontal[m]) * (self.WORKDAY[m] - count_horizontal[m])
-            pena_workday += gap
-
-        if pena_dist:
-            pena_dist = round(pena_dist, 1)
-
-        ret = [("Lv0", pena_desire[0]), ("Lv1", pena_desire[1]), ("Lv2", pena_desire[2]), ("Lv3", pena_desire[3]),
-               ("L/D", pena_seq), ("Over", pena_over), ("Lack", pena_lack),
-               ("Dist", pena_dist), ("Work", pena_workday),
-               ("MaxSeq", max_seq_work), ("MaxOff", max_seq_off)]
-
+    def getResult(self):
+        ret = []
+        first = self.sample_set.record[self.order][0][0]
+        for man in range(self.MANPOWER):
+            tmp = [self.NAME[man]]
+            for day in range(self.DAY):
+                tmp.append(int(first[man * self.DAY + day]))  # ここのint()はjson化するときのために必要
+            ret.append(tmp)
         return ret
 
 
-def upload_img(fn):
-    source_file = "/tmp/{0}".format(fn)
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(fn)
-    blob.upload_from_filename(source_file)
-    blob.make_public()
-    return blob.public_url
+def main(request):
+    request_json = request.get_json()
+    if request_json and "list" in request_json and "param" in request_json:
+        first = optimize(request_json["list"], request_json["param"])
+        return json.dumps({"State": "success", "result": first})
+    else:
+        return json.dumps({"State": "error"})
 
 
-def create_overview_img(model, size: int):
-    keys = []
-    for key in model.getPenalty(model.sample_set.record[model.order][0][0]):
-        keys.append(key[0])
-    sample_list = model.sample_set.record[model.order][:size]
-    data = []
-    for sample in sample_list:
-        penalty = model.getPenalty(sample[0])
-        tmp = []
-        for p in penalty:
-            tmp.append(p[1])
-        data.append(tmp)
-
-    fig = plt.figure(figsize=(5, 3), dpi=240)
-    ax1 = fig.add_subplot(111)
-    ax1.axis('off')
-    ax1.table(cellText=np.array(data).T.astype(int), colLabels=[i for i in range(1, size + 1)],
-              rowLabels=keys, loc="center")
-    fig.tight_layout()
-    fn = "overview_{0}.jpg".format(time.time())
-    plt.savefig("/tmp/{0}".format(fn))
-    plt.close()
-
-    url = upload_img(fn)
-    return url
-
-
-def create_sample_img(model, sample_id):
-    sample = model.sample_set.record[model.order][sample_id][0]
-    const = model.const
-    color = []
-    tag = []
-    clm = ["w" for _ in range(int(model.DAY / 2))]
-    for m in range(model.MANPOWER):
-        tmp_col = []
-        tmp_tag = []
-        for d in range(int(model.DAY / 2)):
-            lunch = sample[model.DAY * m + 2 * d]
-            dinner = sample[model.DAY * m + 2 * d + 1]
-            c_lunch = const[m][2 * d]
-            c_dinner = const[m][2 * d + 1]
-            if lunch:
-                if dinner:
-                    tmp_col.append("lightgreen")
-                    clm[d] = "red"
-                else:
-                    tmp_col.append("orange")
-                    if c_lunch:
-                        clm[d] = "red"
-            else:
-                if dinner:
-                    tmp_col.append("skyblue")
-                    if c_dinner:
-                        clm[d] = "red"
-                else:
-                    tmp_col.append("w")
-            tmp_tag.append("{0} {1}".format(c_lunch, c_dinner))
-        color.append(tmp_col)
-        tag.append(tmp_tag)
-
-    fig = plt.figure(figsize=(7, model.MANPOWER * 0.3), dpi=240)
-    ax1 = fig.add_subplot(111)
-    ax1.axis('off')
-    ax1.table(cellText=np.array(tag), colLabels=[i for i in range(1, int(model.DAY / 2) + 1)],
-              rowLabels=model.NAME, loc="center", cellColours=color, cellLoc="center", colColours=clm)
-    fig.tight_layout()
-    fn = "sample{0}_{1}.jpg".format(sample_id, time.time())
-    plt.savefig("/tmp/{0}".format(fn))
-    plt.close()
-
-    url = upload_img(fn)
-    return url
-
-
-def main(ls, pr):
+def optimize(ls, pr):
     model = ShiftAnneal()
     model.setLIST(data_list=ls)
-    ssl = []
-    for lim in pr[0]:
-        if lim == "":
-            ssl.append(1)
-        else:
-            ssl.append(int(lim))
     model.setParam(des_const=6 * pr[1][0],
                    seq_const=30 * pr[1][1],
                    shift_size_const=30 * pr[1][2],
                    workday_const=1 * pr[1][3],
-                   shift_size_limit=ssl,
+                   shift_size_limit=pr[0],
                    workday=["ignore"],  # setLISTで設定しているから使わない。
-                   num_reads=10)
+                   num_reads=100)
     model.setConst()
     model.sample()
-    # first = model.sample_set.record[model.order][0]
-
-    line_bot_api = LineBotApi(channel_access_token)
-    url_overview = create_overview_img(model, 8)
-    # line_bot_api.push_message(user_id, messages=TextSendMessage())
-    line_bot_api.push_message(user_id, messages=ImageSendMessage(original_content_url=url_overview,
-                                                                 preview_image_url=url_overview))
-
-    url_sample = create_sample_img(model, 0)
-    line_bot_api.push_message(user_id, messages=ImageSendMessage(original_content_url=url_sample,
-                                                                 preview_image_url=url_sample))
-
-
-if __name__ == "__main__":
-    main(ls=[["bob", 0, 1, 2, 0, 2, 1, 0, 2, 3],
-             ["tom", 1, 0, 0, 1, 0, 2, 1, 2, 3],
-             ["sala", 0, 3, 2, 1, 0, 2, 1, 1, 3]],
-         pr=[[1, 1, 1, 1, 1, 1, 1, 1],
-             [4, 5, 3, 1]])
+    return model.getResult()
